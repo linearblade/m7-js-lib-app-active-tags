884577e7c0ad500e14d8125918cc9d96e3a19185
linearblade <sin_vraal@hotmail.com>
2026-02-27 14:19:22 +0800

    kv arguments now supported in pipeline strings

---
+++ examples/inject/fromFile/inject-file-string.js
@@ -0,0 +1,44 @@
+export default {
+    name: "inject-from-file-string",
+    enabled: true,
+    autorun: false,
+    requests: {
+        default: {
+            transport: "http",
+            endpoint: {
+                url: "./fragment.html"
+            },
+            method: "GET",
+            response: {
+                parse: "text",
+                return: "text",
+                requireOk: true
+            }
+        }
+    },
+    pipelines: {
+        original: {
+            run: "@http.send:name=default,url=./fragment.html;@target.find:selector=.inject-content;@target.patch:innerHTML=${buffer:};@target.reset",
+            error: "@error.dump"
+        },
+        new: {
+            run: "@http.send:name=default,url=./fragment-new.html;@target.find:selector=.inject-content;@target.patch:innerHTML=${buffer:};@target.reset",
+            error: "@error.dump"
+        }
+    },
+    events: {
+        original_click: {
+            event: "click",
+            selector: "#loadOriginal",
+            pipeline: "original"
+        },
+        new_click: {
+            event: "click",
+            selector: "#loadNew",
+            pipeline: "new"
+        }
+    },
+    env: {
+        section: "injectFromFileString"
+    }
+};
--- examples/inject/fromFile/inject-file.js
+++ examples/inject/fromFile/inject-file.js
@@ -2,42 +2,79 @@ function injectMarkupIntoTag({ buffer, job, lib } = {}) {
     const host = job && job.e;
     lib.dom.attempt(host, true);
 
+    const slot = host.querySelector(".inject-content");
+    lib.dom.attempt(slot, true);
+
     const html = lib.str.to(buffer.get(), true);
-    lib.dom.set(host, "innerHTML", html);
+    lib.dom.set(slot, "innerHTML", html);
     return true;
 }
 
 export default {
     name: "inject-from-file",
     enabled: true,
-    autorun: true,
-    pipeline: {
-        run: [
-	    "@buffer.dump:test",
-            {
-                op: "@http.send",
-                args: {
-                    adhoc: true,
-                    request: {
-                        transport: "http",
-                        endpoint: {
-                            url: "./fragment.html"
-                        },
-                        method: "GET",
-                        response: {
-                            parse: "text",
-                            return: "text",
-                            requireOk: true
+    autorun: false,
+    requests: {
+        default: {
+            transport: "http",
+            endpoint: {
+                url: "./fragment.html"
+            },
+            method: "GET",
+            response: {
+                parse: "text",
+                return: "text",
+                requireOk: true
+            }
+        }
+    },
+    pipelines: {
+        original: {
+            run: [
+                {
+                    op: "@http.send",
+                    args: {
+                        name: "default",
+                        url: "./fragment.html"
+                    }
+                },
+                injectMarkupIntoTag
+            ],
+            error: [
+                "@error.dump"
+            ]
+        },
+        new: {
+            run: [
+                {
+                    op: "@http.send",
+                    args: {
+                        name: "default",
+                        request: {
+                            endpoint: {
+                                url: "./fragment-new.html"
+                            }
                         }
                     }
-                }
-            },
-	    "@buffer.dump",
-            injectMarkupIntoTag
-        ],
-        error: [
-            "@error.dump"
-        ]
+                },
+                injectMarkupIntoTag
+            ],
+            error: [
+                "@error.dump"
+            ]
+        }
+    },
+    events: {
+        original_click: {
+            event: "click",
+            selector: "#loadOriginal",
+            pipeline: "original"
+        },
+        new_click: {
+            event: "click",
+            selector: "#loadNew",
+            pipeline: "new"
+        }
     },
     env: {
         section: "injectFromFile"
